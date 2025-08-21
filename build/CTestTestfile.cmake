78000a5c0ecf6a62046a5c001e6ec308adb7895a
Yousef40658 <youssefmohamed0585@gmail.com>
2025-08-21 13:05:52 +0300

    starting point

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,17 @@
+# CMake generated Testfile for 
+# Source directory: /home/hod/catkin_ws/src
+# Build directory: /home/hod/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlebot3_manipulation/turtlebot3_manipulation")
+subdirs("turtlebot3_manipulation/turtlebot3_manipulation_description")
+subdirs("turtlebot3_manipulation_simulations/turtlebot3_manipulation_simulations")
+subdirs("open_manipulator_dependencies/roboticsgroup_gazebo_plugins")
+subdirs("turtlebot3_manipulation/turtlebot3_manipulation_bringup")
+subdirs("turtlebot3_manipulation_simulations/turtlebot3_manipulation_gazebo")
+subdirs("turtlebot3_manipulation/turtlebot3_manipulation_gui")
+subdirs("turtlebot3_manipulation/turtlebot3_manipulation_moveit_config")
+subdirs("turtlebot3_manipulation/turtlebot3_manipulation_navigation")
+subdirs("turtlebot3_manipulation/turtlebot3_manipulation_slam")
